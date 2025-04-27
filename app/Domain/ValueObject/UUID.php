493a8f9fb571fe493a84924ef3e25d0bb858c94f
José Silveira <jose.silveira@sblabs.dev>
2025-04-27 17:53:47 -0300

    fix: v4 uuid generation

--- app/Domain/ValueObject/UUID.php
+++ app/Domain/ValueObject/UUID.php
@@ -55,9 +55,17 @@ class UUID
     private static function generate(): string
     {
         $data = random_bytes(16);
+
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
-        return vsprintf('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx', str_split(bin2hex($data), 4));
+        return sprintf(
+            '%08s-%04s-%04s-%04s-%12s',
+            bin2hex(substr($data, 0, 4)),
+            bin2hex(substr($data, 4, 2)),
+            bin2hex(substr($data, 6, 2)),
+            bin2hex(substr($data, 8, 2)),
+            bin2hex(substr($data, 10, 6))
+        );
     }
 }
